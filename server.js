8793a24e4dd107e366ec743dc48a0e6fcaa68b74
nturpin <nova.t974@gmail.com>
2018-06-26 13:02:12 +0400

    affichage avec for

--- server.js
+++ server.js
@@ -8,24 +8,40 @@ server.use(express.static('static'));
 server.set('view engine', 'ejs');
 
 server.get('/', function (req, res) {
-    getrepas(res);   
+    getResultat(function(err, data){
+        if(!err){
+               // res.send(data);
+               var plat = JSON.parse(data);
+                res.render('index', {
+                    repas: plat
+                });
+        }
+        else{
+              res.send(err);
+        }
+
+    });
+
+    var test = "Je suis la page d'accueil";
+
 });
 
+server.get("/repas",function(req, res){
+    res.sendFile(__dirname +"/datas/repas.json");
+});
 
-function getrepas(res){
-    var repas = "Repas 1";
-    var entrymeal = "Salade verte et tomate";
-    var plat = "Pâte carbonnara";
-    var drink1 = "Jus d'orange";
-    var drink2 = "Jus multifruit";
-    var drink3 = "Soda";
-    res.render('index', {
-        repas: repas,
-        entrymeal: entrymeal,
-        drink1: drink1,
-        plat: plat,
-        drink2: drink2,
-        drink3: drink3
+function getResultat(callback){
+    //changer url par l'url de la ressource a recuperer.
+    var url = "http://localhost:'+ port +'/repas";
+    
+    //appel de la fonction request
+    request(url, function(error, response, body) {
+        if (!error && response.statusCode == 200) {
+            result = JSON.stringify(JSON.parse(body));
+            return callback(null, result);
+        } else {
+            return callback(error, null);
+        }
     });
 }
 
